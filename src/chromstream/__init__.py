fb2de8e04ce3dc5f2b3abc752b53dc79c1e66d4d
SRejman <s.rejman@uu.nl>
2025-09-10 14:27:08 +0200

    removed some linting checks, updated init

--- src/chromstream/__init__.py
+++ src/chromstream/__init__.py
@@ -4,8 +4,24 @@ from __future__ import annotations
 
 from importlib.metadata import version
 
-from .parsers import *  
+from .parsers import *
 
 from .objects import *
+
+
+# Import objects first
+from .objects import ChannelChromatograms
+
+# Then import functions that depend on objects
+from .data_processing import integrate_channel, min_subtract, time_window_baseline
+
+__all__ = [
+    "ChannelChromatograms",
+    "integrate_channel",
+    "min_subtract",
+    "time_window_baseline",
+]
+
+
 # Load the version
 __version__ = version("chromstream")
