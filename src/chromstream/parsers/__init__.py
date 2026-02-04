e39c44850f4422f9f7de414b847e8bfdfb4a5519
SRejman <s.rejman@uu.nl>
2026-02-04 17:52:30 +0100

    Dev: Split chromtograms off to seperate dictory and added agilent parser

---
+++ src/chromstream/parsers/__init__.py
@@ -0,0 +1,19 @@
+from .chromeleon import (
+    parse_chromeleon_txt,
+    parse_inject_time,
+    parse_chromatogram_txt,
+    parse_to_channel,
+)
+from .other_files import (
+    parse_MTO_metadata,
+    parse_MTO_asc,
+    detect_log_type,
+    parse_metadata_section,
+    parse_log_MTO,
+    parse_log_type_ft,
+    parse_log_type_hthpir,
+    parse_log_type_lpir,
+    parse_log_type_robert,
+    parse_log_file,
+)
+from .agilent import *
